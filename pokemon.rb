b6da19054b3dba987ef4ca1219e0ae94a63d7293
taichi <taichishiozawa@gmail.com>
2023-09-13 22:51:32 +0900

    ポケモンクラスを定義

--- pokemon.rb
+++ pokemon.rb
@@ -2,5 +2,16 @@
 
 # Pokemonクラスはポケモンに関するクラスです。
 class Pokemon
-  
+  attr_reader :name, :type, :hp
+
+  def initialize(name, type, hp)
+    @name = name
+    @type = type
+    @hp = hp
+  end
+
+  def attack()
+    puts "#{name}のこうげき！"
+  end
+
 end
